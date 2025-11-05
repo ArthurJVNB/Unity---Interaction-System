71ef6414340adb6490f56d742e20e2bfc2f266b5
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-11-05 16:46:12 -0300

    Implemented PushRigidbody (utility behaviour)

---
+++ Assets/Project/Features/InteractionSystem/Runtime/UtilityBehaviours/PushRigidbody.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Project.InteractionSystem
+{
+	public class PushRigidbody : MonoBehaviour
+	{
+		[SerializeField] private Rigidbody _rigidbody;
+		[SerializeField] private Vector3 _force = Vector3.up;
+		[SerializeField] private ForceMode _forceMode = ForceMode.Acceleration;
+		[SerializeField] private bool _autoDisableIsKinematic = true;
+
+		private void Reset()
+		{
+			_rigidbody = GetComponentInChildren<Rigidbody>();
+		}
+
+		public void Push()
+		{
+			if (_autoDisableIsKinematic) _rigidbody.isKinematic = false;
+			_rigidbody.AddForce(_force, _forceMode);
+		}
+
+		public void PushOther(GameObject other)
+		{
+			if (!other) return;
+			if (!other.TryGetComponent(out PushRigidbody otherPush))
+			{
+				otherPush = other.AddComponent<PushRigidbody>();
+				otherPush._rigidbody = other.GetComponentInChildren<Rigidbody>();
+				otherPush._force = _force;
+				otherPush._forceMode = _forceMode;
+			}
+			PushOther(otherPush);
+		}
+
+		public void PushOther(PushRigidbody other)
+		{
+			other.Push();
+		}
+	}
+}
