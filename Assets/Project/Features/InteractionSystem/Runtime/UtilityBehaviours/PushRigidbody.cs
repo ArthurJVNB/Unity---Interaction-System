130ff06cbef48311c5084c7159a3533169d2f35b
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2025-11-06 13:46:32 -0300

    Added option to apply force based on current object's rotation on PushRigidbody

--- Assets/Project/Features/InteractionSystem/Runtime/UtilityBehaviours/PushRigidbody.cs
+++ Assets/Project/Features/InteractionSystem/Runtime/UtilityBehaviours/PushRigidbody.cs
@@ -7,7 +7,10 @@ namespace Project.InteractionSystem
 		[SerializeField] private Rigidbody _rigidbody;
 		[SerializeField] private Vector3 _force = Vector3.up;
 		[SerializeField] private ForceMode _forceMode = ForceMode.Acceleration;
+		[SerializeField] private bool _local;
 		[SerializeField] private bool _autoDisableIsKinematic = true;
+
+		private Vector3 RotatedForce => _local ? (transform.rotation * _force) : _force;
 
 		private void Reset()
 		{
@@ -17,7 +20,7 @@ namespace Project.InteractionSystem
 		public void Push()
 		{
 			if (_autoDisableIsKinematic) _rigidbody.isKinematic = false;
-			_rigidbody.AddForce(_force, _forceMode);
+			_rigidbody.AddForce(RotatedForce, _forceMode);
 		}
 
 		public void PushOther(GameObject other)
@@ -27,9 +30,13 @@ namespace Project.InteractionSystem
 			{
 				otherPush = other.AddComponent<PushRigidbody>();
 				otherPush._rigidbody = other.GetComponentInChildren<Rigidbody>();
-				otherPush._force = _force;
+				otherPush._force = RotatedForce; // apply this object's rotated force
 				otherPush._forceMode = _forceMode;
+				otherPush._local = false; // to apply rotated force based on this object's rotation, and not on the other's rotation
 				otherPush._autoDisableIsKinematic = _autoDisableIsKinematic;
+				otherPush.Push();
+				Destroy(otherPush);
+				return;
 			}
 			PushOther(otherPush);
 		}
